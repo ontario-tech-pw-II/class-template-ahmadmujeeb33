7dc65c56a6651ef88b33d67ded7e2af47c7ba952
ahmadmujeeb33 <60004924+ahmadmujeeb33@users.noreply.github.com>
2020-03-16 23:50:50 -0400

    Add files via upload

--- a6.cpp
+++ a6.cpp
@@ -3,58 +3,60 @@
 #include <iostream>
 using namespace std;
 
-class DynamicArray 
+template<class T> class DynamicArray
 {
 public:
   DynamicArray();
   ~DynamicArray();
-  void addEntry(int newEntry);
-  int getEntry(int index);
+  void addEntry(T newEntry);
+  T getEntry(int index);
   int getSize();
 
 private:
-  int *dynamicArray;
+  T *dynamicArray;
   int size;
 };
 
-DynamicArray::DynamicArray()
+template<class T> DynamicArray<T>::DynamicArray()
 {
   dynamicArray = nullptr;
   size = 0;
 }
 
 
-DynamicArray::~DynamicArray()
+template<class T> DynamicArray<T>::~DynamicArray()
 {
   if (dynamicArray != nullptr)
     delete[] dynamicArray;
 }
 
-int DynamicArray::getSize()
+template<class T> int DynamicArray<T>::getSize()
 {
   return size;
 }
 
-// Adds an entry to the dynamic array. 
-void DynamicArray::addEntry(int newEntry)
+// Adds an entry to the dynamic array.
+template<class T> void DynamicArray<T>::addEntry(T newEntry)
 {
     // Create a new array, copy the contents of the old array, then delete it
-    int *newArray = new int[size + 1];
+    T *newArray = new T[size + 1];
     for (int i = 0; i < size; i++)
     {
     newArray[i] = dynamicArray[i];
     }
-    
+
     delete[] dynamicArray;
-    
+
     dynamicArray = newArray;
-    
+
     // Add the new entry and increment the size
     newArray[size++] = newEntry;
 }
 
+
+
 // Retrieve the element at a given index
-int DynamicArray::getEntry(int index)
+template<class T> T DynamicArray<T>::getEntry(int index)
 {
   if ((index < 0) || (index >= size)){
     cout << "Out of the index !" << endl;
@@ -63,27 +65,6 @@ int DynamicArray::getEntry(int index)
   }
   return dynamicArray[index];
 }
-
-int main()
-{
-  DynamicArray x;
-
-  // Adding entries
-  x.addEntry(7);
-  x.addEntry(5);
-  x.addEntry(4);
-  x.addEntry(2);
-  x.addEntry(8);
-
-  // Output entries
-  cout << "List:" << endl;
-  
-  for (int i = 0; i < x.getSize(); i++)
-    cout << x.getEntry(i) << endl;
-  
-  return 0;
-}
-After changing the class definition to a class template, you can test your class using the following main:
 
 int main()
 {
@@ -98,9 +79,9 @@ int main()
 
   // Output entries
   cout << "List:" << endl;
-  
+
   for (int i = 0; i < x.getSize(); i++)
     cout << x.getEntry(i) << endl;
-  
+
   return 0;
 }
